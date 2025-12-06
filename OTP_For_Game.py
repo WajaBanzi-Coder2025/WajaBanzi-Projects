f8ab9b97ca52ccaccfe5d986281a7bdd36000071
WajaBanzi-Coder2025 <mazetmondisa@gmail.com>
2025-12-06 15:49:32 +0000

    Update OTP_For_Game.py

--- OTP_For_Game.py
+++ OTP_For_Game.py
@@ -3,7 +3,7 @@ from resend import Resend
 import random, time, os
 
 app = Flask(__name__)
-api_key = os.getenv("RESEND_API_KEY")
+resend.api_key = os.getenv("RESEND_API_KEY")
 
 cooldowns = {}
 
@@ -57,6 +57,7 @@ Thanks for supporting WajaBanzi™
         "success": True,
         "otp": otp
     })
+
 
 
 
